88f847984e8733d76b2de9d9f14c67493add7dee
Jonhathan <jonhathan.rodas.gt@camasolympia.com>
2025-08-27 14:12:36 -0600

    remover sap user from secrets

--- amplify/functions/reset-password/resource.ts
+++ amplify/functions/reset-password/resource.ts
@@ -4,8 +4,10 @@ import { secret } from "@aws-amplify/backend";
 export const resetPassword = defineFunction({
   name: "reset-password",
   entry: "./handler.ts",
-  timeoutSeconds: 60,
+  timeoutSeconds: 180,
+  memoryMB: 1024,
   environment: {
     SAP_URL: secret("SAP_URL"),
+    SAP_PASSWORD: secret("SAP_PASSWORD"),
   },
 });
