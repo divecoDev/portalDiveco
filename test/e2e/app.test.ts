e2e1e8cf255a2e7d0e6dce65f5b421032cce5264
Jonhathan <jonhathan.rodas.gt@camasolympia.com>
2025-09-02 17:24:01 -0600

    first test

--- test/e2e/app.test.ts
+++ test/e2e/app.test.ts
@@ -1,4 +1,4 @@
-import { setup, $fetch } from "@nuxt/test-utils/e2e";
+import { setup, createPage } from "@nuxt/test-utils/e2e";
 import { describe, it, expect } from "vitest";
 
 describe("App", async () => {
@@ -7,7 +7,12 @@ describe("App", async () => {
   });
 
   it("should render", async () => {
-    const page = await $fetch("/");
-    expect(page).toContain("DIVECO");
+    const page = await createPage("/");
+    expect(await page.getByTestId("diveco-button").isVisible()).toBe(true);
+  });
+
+  it("should navigate to the login page", async () => {
+    const page = await createPage("/");
+    await page.getByTestId("diveco-button").click();
   });
 });
