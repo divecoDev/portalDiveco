16f9c153c3cc3c600c506bfed5ea6e6d068d0c49
Jonhathan Rodas Lopez <jrodas4044@gmail.com>
2025-09-29 14:27:02 -0600

    refactor: remove unnecessary logging from GetPlanProduccion function and add environment variable logging in GetStatusPipeline and runPipeline functions for improved clarity and debugging

--- amplify/functions/boom/GetStatusPipeline/handler.ts
+++ amplify/functions/boom/GetStatusPipeline/handler.ts
@@ -2,6 +2,27 @@ import {DataFactoryManagementClient} from "@azure/arm-datafactory";
 import { DefaultAzureCredential } from "@azure/identity";
 
 export const handler = async (event: any) => {
+
+  /**
+   * 
+   *        
+   *  Environment variables:
+   *  AZURE_TENANT_ID: secret("AZURE_TENANT_ID"),
+        AZURE_CLIENT_ID: secret("AZURE_CLIENT_ID"),
+        AZURE_CLIENT_SECRET: secret("AZURE_CLIENT_SECRET"),
+        AZURE_SUBSCRIPTION_ID: secret("AZURE_SUBSCRIPTION_ID"),
+        AZURE_DATA_FACTORY_NAME: secret("AZURE_DATA_FACTORY_NAME"),
+  },
+});
+   */
+
+  
+  console.log("AZURE_TENANT_ID", process.env.AZURE_TENANT_ID);
+  console.log("AZURE_CLIENT_ID", process.env.AZURE_CLIENT_ID);
+  console.log("AZURE_CLIENT_SECRET", process.env.AZURE_CLIENT_SECRET);
+  console.log("AZURE_SUBSCRIPTION_ID", process.env.AZURE_SUBSCRIPTION_ID);
+  console.log("AZURE_DATA_FACTORY_NAME", process.env.AZURE_DATA_FACTORY_NAME);
+
   const runId = event.arguments.runId || "";
 
   const resourceGroupName = "ADF";
